2ed36ce54c2276cd507180ebd3df168fd1a82a0f
Adrian Papari <a@onedaybeard.net>
2023-06-11 14:30:35 +0200

    add missing test class

---
+++ core/src/test/kotlin/sift/core/api/testdata/set1/ClassWithExtensionFunction.kt
@@ -0,0 +1,11 @@
+@file:Suppress("UNUSED_PARAMETER")
+
+package sift.core.api.testdata.set1
+
+class ClassWithExtensionFunction {
+
+    fun List<Foo>.hello(foo: Foo): String = TODO()
+
+    class Foo
+}
+
